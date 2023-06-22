94bfabb614bc54acdf4acb2ffe2de59c50ba7721
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-22 14:51:21 -0400

    formatted the date to be more user friendly, added ability to add time for task completion

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const TaskItem = ({ task, handleCheckBox }) => {
+  const { id, title, date, time } = task;
   return (
     <li key={task.id}>
-        <input type="checkbox" onChange={(() => {handleCheckBox(task.id)})}/>
-        {task.title}
-        {task.date}
-
+        <input type="checkbox" onChange={(() => {handleCheckBox(id)})}/>
+        <div>
+          <h3>{title}</h3>
+          <p>Date: {date}</p>
+          <p>Due Time: {time}</p>
+        </div>
     </li>
   )
 }
