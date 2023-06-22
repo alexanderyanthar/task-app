915cfe4384cfae29424a14505af1d871a313b588
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-22 12:44:19 -0400

    added edge case error handling for input, no double tasks and no empty tasks can be submitted

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import TaskItem from './components/TaskItem';
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [taskInput, setTaskInput] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (taskInput.trim() === '') {
+      setError('Please enter a task');
+      return;
+    }
+
+    if (tasks.some((task) => task.title === taskInput)) {
+      setError('Task already exists!');
+      return;
+    }
 
     const newTask = {
       id: Date.now(),
@@ -16,12 +27,13 @@ const App = () => {
     };
 
     setTasks([...tasks, newTask]);
-
     setTaskInput('');
+    setError('');
   }
   
   const handleInputChange = (e) => {
     setTaskInput(e.target.value);
+    setError('');
   }
 
   const handleCheckBox = (taskId) => {
@@ -35,7 +47,8 @@ const App = () => {
 
   return (
     <div className='App'>
-      <TaskForm handleSubmit={handleSubmit} taskInput={taskInput} handleInputChange={handleInputChange} />
+      <TaskForm handleSubmit={handleSubmit} taskInput={taskInput} handleInputChange={handleInputChange} error={error} />
+      {error && <p className='error'>{error}</p>}
       <ul>
         {tasks.map((task) => {
           return (
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TaskForm = ({ handleSubmit, taskInput, handleInputChange }) => {
+const TaskForm = ({ handleSubmit, taskInput, handleInputChange, error }) => {
   return (
     <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter Task' value={taskInput} onChange={handleInputChange}/>
+        <input type="text" placeholder='Enter Task' value={taskInput} onChange={handleInputChange} error={error}/>
     </form>
   )
 }
